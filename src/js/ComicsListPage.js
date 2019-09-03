72c630d6f6160e7c9ff953e600852f986ab1878f
Wojciech Dasiukiewicz <wojtek@octocode.pl>
2019-09-03 15:04:54 +0200

    ComicsListPage component

---
+++ src/js/ComicsListPage.js
@@ -0,0 +1,94 @@
+import React, {Component} from "react";
+import constants from "./constants";
+
+class ComicsListPage extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      comics: []
+    };
+  }
+  componentDidUpdate(prevProps) {
+    if(prevProps.input !== this.props.input){
+      this.loadComics();
+    }
+  }
+
+  loadComics = () => {
+    const { select, input, page, calculateMaxPages } = this.props;
+
+    let now = new Date().getTime();
+    let hash = CryptoJS.MD5(`${now}${constants.PRIVATE_KEY}${constants.API_KEY}`);
+    let marvelApi;
+    if (select === "comics"){
+      marvelApi = `http://gateway.marvel.com/v1/public/${select}?apikey=${constants.API_KEY}&ts=${now}&hash=${hash}&titleStartsWith=${input}&offset=${page}`
+    }
+    if (select === "characters"){
+      marvelApi = `http://gateway.marvel.com/v1/public/${select}?apikey=${constants.API_KEY}&ts=${now}&hash=${hash}&nameStartsWith=${input}&offset=${page}`
+    }
+
+    fetch(marvelApi)
+      .then(resp => resp.json())
+      .then((response)=>{
+        calculateMaxPages(response.data.total);
+        this.setState({
+          comics: response.data.results
+        })
+      })
+      .catch(err => console.error(err));
+  }
+
+  render(){
+    const { comics } = this.state;
+    const { select } = this.props;
+
+    let newList;
+    if (select === "comics"){
+      newList = comics.map((e, index) => (
+        <tr key = {index}>
+          <td> {e.title}</td>
+          <td className="tdNarrow">
+            <img src={e.thumbnail.path + "/portrait_fantastic.jpg"}/>
+          </td>
+          <td className="tdNarrow">
+            <button onClick={this.ownBtnClick} className="button">I have this one</button>
+          </td>
+          <td button onClick={this.toBuyBtnClick} className="tdNarrow">
+            <button className="button">I want to buy it</button>
+          </td>
+        </tr>))
+    } else if (select === "characters"){
+      newList = comics.map((e, index) => (
+        <tr key={index}>
+          <td> {e.name}</td>
+          <td>
+            <img src={e.thumbnail.path + "/portrait_fantastic.jpg"}/>
+          </td>
+          <td className = "seriesTd">
+            <ul>
+            {
+              e.series.items.map((e, index)=>
+                (
+                  <li key={index}>
+                      <p>
+                        {e.name}
+                      </p>
+                  </li>
+                )
+              )
+            }
+            </ul>
+          </td>
+        </tr>))
+    }
+    return (
+      <table className="tableOfResults">
+        <tbody>
+          {newList}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+export default ComicsListPage;
--- src/js/Serach.js
+++ src/js/Serach.js
@@ -1,47 +1,15 @@
 import React, {Component} from "react";
 import ReactDOM from "react-dom";
+import ComicsListPage from "./ComicsListPage";
 
 class Search extends Component{
 
     state = {
         select: "",
         input: "", 
-        data: []
+        maxPages: 0,
+        currentPage: 0,
     }
-
-    // componentDidMount() {
-
-    //     let privateKey = "cf19cc23d18c97ee5c5da91dde4d5648936aff81"
-    //     let apikey = "1e3624de75fcb0f5e9b076008e176f2d";
-    //     let now = new Date().getTime();
-    //     let hash = CryptoJS.MD5(`${now}${privateKey}${apikey}`);
-    //     const marvelApi = `http://gateway.marvel.com/v1/public/comics?apikey=${apikey}&ts=${now}&hash=${hash}`
-
-
-        
-    //     console.log(now)
-
-    //     fetch(marvelApi)
-         
-    //         .then(resp => resp.json())
-    //         .then(marvelAPI => 
-    //          console.log(marvelAPI))
-    //         // .then(function( response ) {
-    //         //     var results = response.data.results;
-    //         //     var resultsLen = results.length;
-    //         //     var output = '<ul>'; 
-                
-    //         //     for(var i=0; i<resultsLen; i++){
-    //         //       if(results[i].images.length > 0) {
-    //         //         var imgPath = results[i].images[0].path + '/standard_xlarge.' + results[i].images[0].extension;
-    //         //         output += '<li><img src="' + imgPath + '"><br>'+results[i].title+'</li>';
-    //         //       }
-    //         //     }  
-    //         //     output += '</ul>'
-    //         //     $('#results').append(output);
-    //         // })
-    //         .catch(err => console.error(err));
-    // }
 
     handleChange = (e) => {
         this.setState({
@@ -57,32 +25,7 @@ class Search extends Component{
 
     handleSubmit = (e) => {
         e.preventDefault();
-        let privateKey = "cf19cc23d18c97ee5c5da91dde4d5648936aff81"
-        let apikey = "1e3624de75fcb0f5e9b076008e176f2d";
-        let now = new Date().getTime();
-        let hash = CryptoJS.MD5(`${now}${privateKey}${apikey}`);
-        const select = this.state.select;
-        const input = this.state.input;
-        let marvelApi; 
-        if (select === "comics"){
-            marvelApi = `http://gateway.marvel.com/v1/public/${select}?apikey=${apikey}&ts=${now}&hash=${hash}&titleStartsWith=${input}`
-        }
-        if (select === "characters"){
-            marvelApi = `http://gateway.marvel.com/v1/public/${select}?apikey=${apikey}&ts=${now}&hash=${hash}&nameStartsWith=${input}`
-        }
-        
-        console.log(now)
-        
-        fetch(marvelApi)
-         
-            .then(resp => resp.json())
-            .then((response)=>{
-                console.log(response.data.results)
-                this.setState({
-                data: response.data.results
-            })
-            })
-            .catch(err => console.error(err));
+
     }
 
     ownBtnClick = () =>{
@@ -93,40 +36,71 @@ class Search extends Component{
         console.log("click")
     }
 
+    calculateMaxPagesHandler = allItemsCount => {
+        const maxPages = Math.floor(allItemsCount / 20);
+        this.setState({
+            maxPages
+        });
+    }
+
+    renderComicsLists = () => {
+        const { maxPages, select, input } = this.state;
+        const comicsLists = [];
+        if(maxPages > 0){
+            for(let i=0; i < maxPages; i++){
+                comicsLists.push(
+                  <ComicsListPage
+                    calculateMaxPages={this.calculateMaxPagesHandler}
+                    page={i}
+                    select={select}
+                    input={input}
+                  />
+                )
+            }
+        }else{
+            comicsLists.push(
+              <ComicsListPage
+                calculateMaxPages={this.calculateMaxPagesHandler}
+                page={0}
+                select={select}
+                input={input}
+              />
+            )
+        }
+        return comicsLists;
+    }
+
     render(){
-        let newList;
-        if (this.state.select === "comics"){
-            newList = this.state.data.map((e, index) => (<tr key = {index}><td> {e.title}</td><td className = "tdNarrow"><img src = {e.thumbnail.path + "/portrait_fantastic.jpg"}/></td><td className = "tdNarrow"><button onClick = {this.ownBtnClick} className = "button">I have this one</button></td> <td button onClick = {this.toBuyBtnClick} className = "tdNarrow"><button className = "button">I want to buy it</button></td> </tr>))
-        }
-        else if (this.state.select === "characters"){
-            newList = this.state.data.map((e, index) => (<tr key = {index}><td> {e.name}</td><img src = {e.thumbnail.path + "/portrait_fantastic.jpg"}/><td className = "seriesTd">{e.series.items.map((e, index)=> (<tr key = {index} ><td >{e.name}</td></tr>))}</td></tr>))
-        }
-        
         let error;
         if (this.state.input.length < 3 || this.state.select === ""){
-            error = <div className = "searchError"><h1>{localStorage.getItem("name")}, you need to tell me what are you looking for</h1></div>
+            error = (
+              <div className = "searchError">
+                  <h1>{localStorage.getItem("name")}, you need to tell me what are you looking for</h1>
+              </div>
+            );
         }
 
         return(
             <>
-            {error}
-            <div className = "searchContainer">                
-                <form onSubmit = {this.handleSubmit}>
-                    <select value= {this.state.select} onChange ={this.handleChange} name = "search by" className = "searchContainer-select">
-                        <option value="">Choose category</option>
-                        <option value = "characters">Characters</option>
-                        <option value = "comics">Comics title</option>
-                        <option></option>
-                    </select>
-                    <input value = {this.state.input} onChange = {this.handleInputChange} placeholder = "Enter what you search for" className = "sarchContainer-input"></input>
-                    <input type = "submit" className = "button" value = "SEARCH"></input>
-                </form>
-            </div>
-            <table className = "tableOfResults">
-                <tbody>
-                    {newList}
-                </tbody>
-            </table>
+                {error}
+                <div className = "searchContainer">
+                    <form onSubmit = {this.handleSubmit}>
+                        <select
+                          value={this.state.select}
+                          onChange={this.handleChange}
+                          name="search by"
+                          className="searchContainer-select"
+                        >
+                            <option value="">Choose category</option>
+                            <option value = "characters">Characters</option>
+                            <option value = "comics">Comics title</option>
+                            <option></option>
+                        </select>
+                        <input value = {this.state.input} onChange = {this.handleInputChange} placeholder = "Enter what you search for" className = "sarchContainer-input"></input>
+                        <input type = "submit" className = "button" value = "SEARCH"></input>
+                    </form>
+                </div>
+                { this.renderComicsLists() }
             </>
         )
     }
